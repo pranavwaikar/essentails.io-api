91233721fa96f3d99fc58acf6b42bcff6d3478f4
Kajal Mohite <kajal.mohite@globant.com>
2020-03-29 01:25:58 +0530

    checking GET /

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :consumers, :serviceproviders
+      get '/', to: proc { [200, {}, ['']] }
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
